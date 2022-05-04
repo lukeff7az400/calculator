bcc1b02b9d439a51cf7455b100979553f3bc0cd6
lukeff7az400 <104909838+lukeff7az400@users.noreply.github.com>
2022-05-04 10:45:09 -0700

    Actually no, it isn't! You didn't test, lolNOOB

--- api/controllers/arithmeticController.js
+++ api/controllers/arithmeticController.js
@@ -11,7 +11,8 @@ exports.calculate = function(req, res) {
   });
 
   var operations = {
-    'add':      function(a,b) { return a + b },
+    'add':      function(a,b) { return +a + +b },
+    //You have to cast it to int, else it concats a string, noob!
     'subtract': function(a,b) { return a - b },
     'multiply': function(a,b) { return a * b },
     'divide':   function(a,b) { return a / b },
